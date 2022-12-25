50fec9c82f437a083f88d2a6b5b60a8eced736b8
saurabhnegi24798 <saurabhnegi24798@gmail.com>
2022-12-25 20:50:54 +0530

    frontend

---
+++ build/precache-manifest.283c8bfa08bd1c48b8e630d44fe9f937.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b00fb8711c165ec0bd4be40cdad921e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b21e1dff9e51c6e0cf9e",
+    "url": "/static/css/main.2988cb80.chunk.css"
+  },
+  {
+    "revision": "478603ddaccdb6f55609",
+    "url": "/static/js/2.8d545eff.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8d545eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b21e1dff9e51c6e0cf9e",
+    "url": "/static/js/main.9fa6f8c7.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
